e55026532696bb0945257337dbc3f417dd41da27
EriEriel <gwynpond@gmail.com>
2026-03-26 00:51:28 +0700

    OAuth for Github but currently hit server issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { prisma } from "@/lib/db";
+import { prisma } from "@/lib/prisma";
 import AddEntryModal from "@/components/AddEntryModal";
 import EntryCard from "@/components/EntryCard";
 import { SearchBar } from "@/components/SearchBar";
--- src/components/actions.ts
+++ src/components/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { prisma } from "@/lib/db";
+import { prisma } from "@/lib/prisma";
 import { redirect } from "next/navigation";
 import { Category, Status } from "@/generated/prisma/enums";
 
